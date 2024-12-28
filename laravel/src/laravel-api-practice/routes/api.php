50aaa64dc189dcf0970dfab2b2528476fb7b8dd8
yaharu711 <yah7yah707@gmail.com>
2024-12-28 10:38:38 +0900

    add delete api

--- laravel/src/laravel-api-practice/routes/api.php
+++ laravel/src/laravel-api-practice/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CreateTodoController;
+use App\Http\Controllers\DeleteTodoController;
 use App\Http\Controllers\GetHelloMessageController;
 use App\Http\Controllers\GetTodosController;
 use App\Http\Controllers\UpdateTodoController;
@@ -10,5 +11,6 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('/hello-message', GetHelloMessageController::class);
     Route::post('/todo', CreateTodoController::class);
     Route::get('/todo', GetTodosController::class);
-    Route::post('/todo/{id}', UpdateTodoController::class);
+    Route::patch('/todo/{id}', UpdateTodoController::class);
+    Route::delete('/todo/{id}', DeleteTodoController::class);
 });
