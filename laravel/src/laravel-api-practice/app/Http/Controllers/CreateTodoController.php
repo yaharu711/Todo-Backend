4def60ef6842b85f256932348e494adfdbe4d2f8
yaharu711 <yah7yah707@gmail.com>
2025-01-29 22:34:51 +0900

    add update sort todo in create api

--- laravel/src/laravel-api-practice/app/Http/Controllers/CreateTodoController.php
+++ laravel/src/laravel-api-practice/app/Http/Controllers/CreateTodoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\CreateTodoRequest;
 use DateTimeImmutable;
+use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -18,12 +19,27 @@ class CreateTodoController extends Controller
         $now = new DateTimeImmutable();
         $user_id = Auth::user()->id;
         $todoName = $request->input('name');
-        DB::table('todos')->insert([
-            'name' => $todoName,
-            'created_at' => $now->format('Y-m-d H:i:s'),
-            'imcompleted_at' => $now->format('Y-m-d H:i:s'),
-            'user_id' => $user_id,
-        ]);
+        try {
+            DB::beginTransaction();
+            // Todoの作成
+            $created_todo_id = DB::table('todos')->insertGetId([
+                'name' => $todoName,
+                'created_at' => $now->format('Y-m-d H:i:s'),
+                'imcompleted_at' => $now->format('Y-m-d H:i:s'),
+                'user_id' => $user_id,
+            ]);
+            // Todoの順序を保存→初回のことを考慮してupsertにしている
+            DB::statement("
+                INSERT INTO imcompleted_todo_orders (user_id, imcompleted_todo_order)
+                VALUES (?, ARRAY[?]::int[])
+                ON CONFLICT (user_id)
+                DO UPDATE SET imcompleted_todo_order = ARRAY[?]::int[] || imcompleted_todo_orders.imcompleted_todo_order
+            ", [$user_id, $created_todo_id, $created_todo_id]);
+            DB::commit();
+        } catch (Exception $exception) {
+            DB::rollBack();
+            throw $exception;
+        }
 
         return response()->json([
             'message' => 'success'
