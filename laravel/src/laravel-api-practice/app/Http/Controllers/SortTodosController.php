32e3951a3fd9a6cd7b12195c8b17f9cfac7e1b2b
yaharu711 <yah7yah707@gmail.com>
2025-01-31 16:04:56 +0900

    refactoring

--- laravel/src/laravel-api-practice/app/Http/Controllers/SortTodosController.php
+++ laravel/src/laravel-api-practice/app/Http/Controllers/SortTodosController.php
@@ -14,9 +14,9 @@ class SortTodosController extends Controller
      */
     public function __invoke(SortTodosRequest $request): JsonResponse
     {
-        $user_id = Auth::user()->id;
+        $user_id = Auth::id();
         $todos_order = $request->input('todos_order');
-        // ちゃんとしたいななら、存在確認や重複していないことを確認もできるがそこまでしなくて良いかな
+
         DB::table('imcompleted_todo_orders')
             ->where('user_id', $user_id)
             // Postgresが扱える配列の形{}に変換する
