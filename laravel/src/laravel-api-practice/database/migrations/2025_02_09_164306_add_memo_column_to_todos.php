1f69dc6d285ecccc8e89ee12d4f2c7150b5e1e27
yaharu711 <yah7yah707@gmail.com>
2025-02-09 16:57:26 +0900

    add migration

---
+++ laravel/src/laravel-api-practice/database/migrations/2025_02_09_164306_add_memo_column_to_todos.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('todos', function (Blueprint $table) {
+            $table->string('memo', 3000)->nullable(false)->default('');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('todos', function (Blueprint $table) {
+            $table->dropColumn('memo');
+        });
+    }
+};
