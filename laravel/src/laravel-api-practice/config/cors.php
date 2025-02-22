368453205653acfc46e43e5b1bd9d111e001ecce
yaharu711 <yah7yah707@gmail.com>
2025-02-22 14:54:10 +0900

    add setting

--- laravel/src/laravel-api-practice/config/cors.php
+++ laravel/src/laravel-api-practice/config/cors.php
@@ -10,6 +10,6 @@
        'allowed_headers' => ['*'],
         // 許可するリクエストオリジンの設定
         //`*`かオリジンに完全一致、またはワイルドカードが利用可。
-       'allowed_origins' => ['http://localhost:5173', "https://www.practice-react-laravel.site"],
+       'allowed_origins' => ['http://localhost:5173', "http://dev.practice-react-laravel.site:5173", "https://www.practice-react-laravel.site"],
        'supports_credentials' => true,
     ];
