4d0f95b068494cf03806e91db58c94e59fac613e
yaharu711 <yah7yah707@gmail.com>
2025-03-20 12:56:00 +0900

    add check exist valid fcm token api and modify invalidate api

--- laravel/src/laravel-api-practice/routes/api.php
+++ laravel/src/laravel-api-practice/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CheckExistValidFcmToken;
 use App\Http\Controllers\CheckHealthController;
 use App\Http\Controllers\CreateTodoController;
 use App\Http\Controllers\DeleteTodoController;
@@ -19,6 +20,7 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::put('/todos/sort', SortTodosController::class); // 並び順はリクエストのたびにリクエストされたもので置き換えるので冪等という意味でput
     Route::post('/fcm', SaveFcmTokenController::class);
     Route::post('fcm/invalidate', InvalidateLatestFcmTokenController::class); // fcmトークンは一応残しておきたいから論理削除みたいにしている。
+    Route::get('/fcm/check-exist-valid-token', CheckExistValidFcmToken::class);
     Route::post('/push', ForTestPushNotificationToWebController::class);
 });
 
