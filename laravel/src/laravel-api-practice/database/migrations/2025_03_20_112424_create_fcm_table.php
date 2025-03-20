37746e7b7cc232747528eaeb10edb62590ce86be
yaharu711 <yah7yah707@gmail.com>
2025-03-20 12:10:01 +0900

    modify and add table

--- laravel/src/laravel-api-practice/database/migrations/2025_03_20_112424_create_fcm_table.php
+++ laravel/src/laravel-api-practice/database/migrations/2025_03_20_112424_create_fcm_table.php
@@ -15,9 +15,10 @@ return new class extends Migration
             // user_idカラムを追加
             $table->unsignedBigInteger('user_id');
             $table->text('token');
-            $table->timestamp('created_at');
+            $table->timestamp('created_at')->now();
 
-            // 外部キー制約を追加
+            // 全てが無効になるわけではなく、デバイスごとにもトークンは発行されるため、user_idとtokenがpkとなる
+            $table->primary(['user_id', 'token']);
             $table->foreign('user_id')->references('id')->on('users')->OnDelete('cascade');
         });
     }
