9e6fe6f2a6d0add5614748ddc637c9bcccd589e1
yaharu711 <yah7yah707@gmail.com>
2025-03-20 09:51:36 +0900

    add save fcm token api

--- laravel/src/laravel-api-practice/routes/api.php
+++ laravel/src/laravel-api-practice/routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\CreateTodoController;
 use App\Http\Controllers\DeleteTodoController;
 use App\Http\Controllers\GetTodosController;
 use App\Http\Controllers\ForTestPushNotificationToWebController;
+use App\Http\Controllers\SaveFcmTokenController;
 use App\Http\Controllers\SortTodosController;
 use App\Http\Controllers\UpdateTodoController;
 use Illuminate\Support\Facades\Route;
@@ -15,6 +16,7 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::patch('/todos/{id}', UpdateTodoController::class);
     Route::delete('/todos/{id}', DeleteTodoController::class);
     Route::put('/todos/sort', SortTodosController::class); // 並び順はリクエストのたびにリクエストされたもので置き換えるので冪等という意味でput
+    Route::post('/fcm', SaveFcmTokenController::class);
     Route::post('/push', ForTestPushNotificationToWebController::class);
 });
 
