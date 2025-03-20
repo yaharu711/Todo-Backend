eb840a3061dc992edd4842cbbabbb8402a7f2df8
yaharu711 <yah7yah707@gmail.com>
2025-03-20 16:56:33 +0900

    modify deploy script

--- laravel/prod/scripts/laravel-deploy.sh
+++ laravel/prod/scripts/laravel-deploy.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
-chmod -R 755 /etc/secrets/
+mkdir -p /var/www/storage/app/secrets
+# 所有権をLaravelの実行ユーザ（例えば www-data）に変更する
+chown -R www-data:www-data /var/www/storage/app
+# 「/etc/secrets配下はアプリケーション側から読み込みできない」かつ「chmodなどで権限の変更もできない」ため、別の場所にコピーする
+cp /etc/secrets/firebase_root_service_account_private_file.json /var/www/storage/app/secrets/firebase_root_service_account_private_file.json
+# パーミッション設定 (所有者のみが読み書きできる)
+chmod 600 /var/www/storage/app/secrets/firebase_root_service_account_private_file.json
 
 set -ex
 echo "Running composer"
