ca5a777b89e5dd06678f96ba4cc63a9e6c96aeb4
yaharu711 <yah7yah707@gmail.com>
2025-04-02 19:19:40 +0900

    delete dd

--- laravel/src/laravel-api-practice/app/PushNotification/Handlers/FcmNotificationResultHandler.php
+++ laravel/src/laravel-api-practice/app/PushNotification/Handlers/FcmNotificationResultHandler.php
@@ -90,7 +90,7 @@ class FcmNotificationResultHandler implements NotificationResultHandlerInterface
                 DB::commit();
             } catch (Exception $exception) {
                 DB::rollBack();
-                dd($exception);
+                // エラー通知する（slackかな）
             }
         }
         
