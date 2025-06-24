00b4bdd0259430734ace3171f2be24a4a57969a5
yaharu711 <yah7yah707@gmail.com>
2025-06-24 15:33:24 +0900

    add upsertAsFirst()

--- laravel/src/laravel-api-practice/app/Repositories/ImcompletedTodoOrderRepository.php
+++ laravel/src/laravel-api-practice/app/Repositories/ImcompletedTodoOrderRepository.php
@@ -5,13 +5,14 @@ use Illuminate\Support\Facades\DB;
 
 class ImcompletedTodoOrderRepository
 {
-    public function insertAsFirst(int $user_id, int $todo_id): void
+    public function upsertAsFirst(int $user_id, int $todo_id): void
     {
         DB::statement('
-            UPDATE imcompleted_todo_orders 
-            SET imcompleted_todo_order = ARRAY[?]::int[] || imcompleted_todo_orders.imcompleted_todo_order
-            WHERE user_id = ?
-        ', [$todo_id, $user_id]);
+            INSERT INTO imcompleted_todo_orders (user_id, imcompleted_todo_order)
+                VALUES (?, ARRAY[?]::int[])
+                ON CONFLICT (user_id)
+                DO UPDATE SET imcompleted_todo_order = ARRAY[?]::int[] || imcompleted_todo_orders.imcompleted_todo_order
+        ', [$user_id, $todo_id, $todo_id]); 
     }
 
     public function delete(int $user_id, int $todo_id): void
